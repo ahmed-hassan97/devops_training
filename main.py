9e061c0100e3391d042710e204749a2d3bcf4ce8
ahmed.gaber <ahmed.gaber@gizasystems.com>
2023-06-07 12:37:33 +0300

    add some modification in feature1

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("you add database connection")
     print("solving issue in Feature1")
+    print("i change some modified")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
